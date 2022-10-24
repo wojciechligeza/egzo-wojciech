b3205f11ea0aaa9f9d3a0b33da7446eaff2916c2
wojLi <wojciech.ligeza@icloud.com>
2022-10-24 14:10:28 +0200

    build: setup project

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+/** @type { import("@types/eslint").Linter.BaseConfig } */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': 'error',
+  },
+}
---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import reactLogo from './assets/react.svg'
+
+function App() {
+  return (
+    <div>
+      <div>
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1 className="text-gray-500">Vite + React</h1>
+    </div>
+  )
+}
+
+export default App
